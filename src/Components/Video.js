813a23a043846bad02681168379488c0276f351e
loulou-gif <konankanjulius10@gmail.com>
2024-02-18 21:48:39 +0000

    end video wireframe

---
+++ src/Components/Video.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Video = () => {
+    return (
+        <section className='flex justify-center items-center h-[850px] '>
+            <div className='bg-white w-9/12 h-[750px] '>
+                <div className='search-ep  bg-slate-400 w-full h-16'></div>
+                <div className='search-ep  w-full h-[622px]'></div>
+                <div className='search-ep  bg-slate-400 w-full h-16'></div>
+            </div>
+        </section>
+    );
+};
+
+export default Video;
--- src/pages/public/Streams.js
+++ src/pages/public/Streams.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import NavBar from '../../Components/Navbar';
 import Details from '../../Components/Details';
+import Video from '../../Components/Video';
+import Comments from '../../Components/Comments';
 
 const Streams = () => {
     return (
         <section className='streams'>
             <NavBar/>
             <Details/>
+            <Video/>
+            <Comments/>
         </section>
     );
 };
