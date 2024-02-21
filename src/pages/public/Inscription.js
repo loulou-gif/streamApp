7420d0aa4844b956291f2ed698e156572f9edb3f
loulou-gif <konankanjulius10@gmail.com>
2024-02-21 23:25:17 +0000

    last

--- src/pages/public/Inscription.js
+++ src/pages/public/Inscription.js
@@ -7,11 +7,7 @@ import img from '../../Assets/img/Rectangle 123.png'
 const Inscription = () => {
     return (
         <section className='sect-inscription flex'>
-        <div className='img w-3/5  h-screen' style={{
-                backgroundImage:`url(${img})`,
-                backgroundPosition:'center',
-                backgroundRepeat:'no-repeat',
-            }}></div>
+        <div className='img w-3/5  h-screen'><img alt=''src={img} className='w-full h-full'/></div>
         <div className='flex justify-center items-center pt-10  w-2/5'>
             <div className='form flex items-center h-[605px]  flex-col'>
                 <div className='circle rounded-full  h-[150px] w-[150px] bg-blue-300'></div>
