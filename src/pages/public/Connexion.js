bdde3a6bd55045b02b23053f13458b21b667c64c
unknown <konankanjulius10@gmail.com>
2024-02-29 17:53:31 +0000

    api création compte

--- src/pages/public/Connexion.js
+++ src/pages/public/Connexion.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../Assets/Css/styles.css'
 import { Link } from 'react-router-dom';
 import img from '../../Assets/img/Rectangle 133.png'
+import axios from 'axios';
 
 const Connexion = () => {
+    const [sign, setSign] = useState({
+        username:"",
+        email:"",
+        password:"",
+    })
+
+    const handleInputChange = (event) =>{
+        const {name,value} = event.target;
+        setSign({ ...sign, [name]: value});
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        axios.post("http://localhost:5000/user", sign)
+        .then(response =>{
+            console.log('user create succesfully', response.data);
+            console.log(sign)
+            setSign({
+              username:"",
+              email:"",
+              password:"",
+              // ... réinitialisez d'autres champs si nécessaire
+          });
+        })
+        .catch(error => {
+            console.error("ERROR: Can't create user", error)
+            console.log(sign)
+        })
+    }
+    
+
     return (
         <section className='flex'>
-            <div className='img w-4/5 bg-blue-500 h-screen'><img alt='' src={img} className='w-full h-full'/> </div>
+            <div className='img w-4/5 bg-blue-500 h-screen'><img alt='' src={img} className='w-full h-full'/></div>
             <div className='flex justify-center items-center pt-10  w-2/5'>
                 <div className='form flex items-center h-[605px]  flex-col'>
                     <div className='circle rounded-full  h-[150px] w-[150px] bg-blue-300'></div>
-                    <form className='connexion-form flex-col  m-10 w-[450px]'>
+                    <form className='connexion-form flex-col  m-10 w-[450px]' onSubmit={handleSubmit}>
                     <h3>Pas de compte?<Link to='/inscription' className='link'> S'inscrire</Link> </h3>
-                        <input type='text' className='mt-5 mb-5 pl-5 p-2 h-[60px] rounded-xl' placeholder="Nom d'utilisateur"/>
-                        <input type='password' className=' h-[60px] w-[342]  mb-5 pl-5 p-2 rounded-xl' placeholder='Mot de passe'/>
+                        <input type='text' className='mt-5 mb-5 pl-5 p-2 h-[60px] rounded-xl' placeholder="Nom d'utilisateur" onChange={handleInputChange}/>
+                        <input type='password' className=' h-[60px] w-[342]  mb-5 pl-5 p-2 rounded-xl' placeholder='Mot de passe' />
                         <button className='boutton bg-blue-700 text-white rounded-xl h-[60px] w-[342] ' >Se connecter</button>
                     </form>
                 </div>
--- src/pages/public/Inscription.js
+++ src/pages/public/Inscription.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../Assets/Css/styles.css'
 import { Link } from 'react-router-dom';
 import img from '../../Assets/img/Rectangle 123.png'
+import axios from 'axios';
 
 const Inscription = () => {
+    const [sign, setSign] = useState({
+        username:"",
+        email:"",
+        password:"",
+    })
+
+    const handleInputChange = (event) =>{
+        const {name,value} = event.target;
+        setSign({ ...sign, [name]: value});
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        axios.post("http://localhost:5000/user", sign)
+        .then(response =>{
+            console.log('user create succesfully', response.data);
+            console.log(sign)
+            setSign({
+              username:"",
+              email:"",
+              password:"",
+              // ... réinitialisez d'autres champs si nécessaire
+          });
+        })
+        .catch(error => {
+            console.error("ERROR: Can't create user", error)
+            console.log(sign)
+        })
+    }
+
     return (
         <section className='sect-inscription flex'>
         <div className='img w-4/5  h-screen'><img alt=''src={img} className='w-full h-full'/></div>
         <div className='flex justify-center items-center pt-10  w-2/5'>
             <div className='form flex items-center h-[605px]  flex-col'>
                 <div className='circle rounded-full  h-[150px] w-[150px] bg-blue-300'></div>
-                <form className='connexion-form flex-col  m-10 w-[450px]'>
+                <form className='connexion-form flex-col  m-10 w-[450px]' onSubmit={handleSubmit}>
                 <h3>Déjà un compte?<Link to='/connexion' className='link'> Se connecter</Link> </h3>
-                    <input type='text' name='username' className='mt-5 pl-5 p-2 h-[60px] rounded-xl' placeholder="Nom d'utilisateur"/>
-                    <input type='email' name='email' className='mt-5 mb-5 pl-5 p-2 h-[60px] rounded-xl' placeholder="Email"/>
-                    <input type='password' className='h-[60px] w-[342]  mb-5 pl-5 p-2 rounded-xl' placeholder='Mot de passe'/>
-                    <button className='boutton bg-blue-700 text-white h-[60px] rounded-xl w-[342] ' >S'inscrire</button>
+                    <input type='text' name='username' className='mt-5 pl-5 p-2 h-[60px] rounded-xl' placeholder="Nom d'utilisateur" onChange={handleInputChange}/>
+                    <input type='email' name='email' className='mt-5 mb-5 pl-5 p-2 h-[60px] rounded-xl' placeholder="Email" onChange={handleInputChange}/>
+                    <input type='password' name='password' className='h-[60px] w-[342]  mb-5 pl-5 p-2 rounded-xl' placeholder='Mot de passe' onChange={handleInputChange}/>
+                    <button className='boutton bg-blue-700 text-white h-[60px] rounded-xl w-[342px] ' >S'inscrire</button>
                 </form>
             </div>
         </div>
